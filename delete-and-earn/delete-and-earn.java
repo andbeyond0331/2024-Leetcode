e6c0135a54a82bc0a49f861bc276dfc2157e9931
PARK MI JUNG <88870452+andbeyond0331@users.noreply.github.com>
2024-04-02 21:10:02 +0900

    Time: 6 ms, Memory: 45.4 MB - LeetHub

---
+++ delete-and-earn/delete-and-earn.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int deleteAndEarn(int[] nums) {
+        int answer = -1;
+        int[] count = new int[10001];
+        for(int i = 0; i < nums.length; i++) {
+            count[nums[i]]++;
+        }
+        int[] max = new int[10001];
+        max[1] = count[1]*1;
+        max[2] = Math.max(count[2]*2, count[1]);
+        for(int i = 3; i < max.length; i++) {
+            max[i] = Math.max(max[i-1], max[i-2] + count[i]*i);
+            answer = Math.max(max[i], answer);
+        }
+        return answer;
+    }
+}
