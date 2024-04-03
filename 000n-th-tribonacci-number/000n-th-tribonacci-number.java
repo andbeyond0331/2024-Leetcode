932007e35ecfb7e3980484fbc56484ea0cca9884
PARK MI JUNG <88870452+andbeyond0331@users.noreply.github.com>
2024-04-03 15:13:00 +0900

    Time: 0 ms, Memory: 40.1 MB - LeetHub

--- 000n-th-tribonacci-number/000n-th-tribonacci-number.java
+++ 000n-th-tribonacci-number/000n-th-tribonacci-number.java
@@ -1,34 +1,23 @@
-// 2차 풀이 240403 top-down
+// 2차 풀이 240403 bottom-up
 
 class Solution {
-    static int[] dp;
-    static boolean[] visited;
     public int tribonacci(int n) {
-        dp = new int[n+1];
-        visited = new boolean[n+1];
+        int[] memory = new int[n+1];
+        memory[0] = 0;
         if (n == 0) {
             return 0;
         }
-        dp[0] = 0;
-        visited[0] = true;
+        memory[1] = 1;
         if (n == 1) {
             return 1;
         }
-        dp[1] = 1;
-        visited[1] = true;
         if (n == 2) {
             return 1;
         }
-        dp[2] = 1;
-        visited[2] = true;
-        return memorize(n);
-    }
-    static int memorize(int target) {
-        if(visited[target]) {
-            return dp[target];
+        memory[2] = 1;
+        for(int i = 3; i <= n; i++) {
+            memory[i] = memory[i-1] + memory[i-2] + memory[i-3];
         }
-        dp[target] = memorize(target-1) + memorize(target-2) + memorize(target-3);
-        visited[target] = true;
-        return dp[target];
+        return memory[n];
     }
 }
