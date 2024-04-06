03f71a1733d8242ebc8e28768fbc92edf8790089
PARK MI JUNG <88870452+andbeyond0331@users.noreply.github.com>
2024-04-06 17:16:32 +0900

    Time: 11 ms, Memory: 54.8 MB - LeetHub

--- maximum-score-from-performing-multiplication-operations/maximum-score-from-performing-multiplication-operations.java
+++ maximum-score-from-performing-multiplication-operations/maximum-score-from-performing-multiplication-operations.java
@@ -1,28 +1,27 @@
 class Solution {
-    private static int n;
-    private static int m;
-    private static int[] multipliers;
-    private static int[] nums;
-    private static int[][] memo;
+    // 혼자 풀어보기 24.04.06
+    static int[] nums;
+    static int[] multipliers;
+    static int n;
+    static int m;
+    static int[][] memo;
     public int maximumScore(int[] nums, int[] multipliers) {
-        this.n = nums.length;
-        this.m = multipliers.length;
         this.nums = nums;
         this.multipliers = multipliers;
-        this.memo = new int[m][m];
+        n = nums.length;
+        m = multipliers.length;
+        memo = new int[m][m];
         return dp(0, 0);
     }
-    
-    private int dp(int i, int left) {
+    static int dp(int i, int left) {
         if(i == m) {
             return 0;
         }
-        int mul = multipliers[i];
+        int temp = multipliers[i];
         int right = n - 1 - (i - left);
         if(memo[i][left] == 0) {
-            memo[i][left] = Math.max(mul * nums[left] + dp(i + 1, left + 1), mul * nums[right] + dp(i + 1, left));
+            memo[i][left] = Math.max(temp * nums[left] + dp(i + 1, left + 1), temp * nums[right] + dp(i + 1, left));
         }
         return memo[i][left];
     }
-    
 }
