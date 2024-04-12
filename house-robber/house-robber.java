6ccdf86c01d8cdb1f2b265b8fbf1c484fffec5df
PARK MI JUNG <88870452+andbeyond0331@users.noreply.github.com>
2024-04-12 21:37:21 +0900

    Time: 0 ms, Memory: 41.1 MB - LeetHub

--- house-robber/house-robber.java
+++ house-robber/house-robber.java
@@ -1,21 +1,25 @@
 class Solution {
     public int rob(int[] nums) {
-        // 2차 풀이 2024.04.03
+        // 3차 풀이 2024.04.12
         int len = nums.length;
-        int[] max = new int[len];
-        max[0] = nums[0];
-        
-        if(len==1) {
+        if(len == 1) {
             return nums[0];
         }
-        max[1] = nums[1];
-        if(len==2) {
+        int[] maxAmount = new int[len];
+        maxAmount[0] = nums[0];
+        if(len == 2) {
             return Math.max(nums[0], nums[1]);
         }
-        max[2] = Math.max(max[0]+nums[2], max[1]);
-        for(int i = 3; i < len; i++) {
-            max[i] = Math.max(Math.max(max[i-1], max[i-3] + nums[i]), max[i-2] + nums[i]);
+        maxAmount[1] = nums[1];
+        if(len == 3) {
+            return Math.max(nums[1], nums[0]+nums[2]);
         }
-        return Math.max(max[len-1], max[len-2]);
+        maxAmount[2] = Math.max(nums[1], nums[0] + nums[2]);
+        int answer = -1;
+        for(int i = 3; i < len; i++) {
+            maxAmount[i] = Math.max(maxAmount[i-1], Math.max(maxAmount[i-2], maxAmount[i-3]) + nums[i]);
+            answer = Math.max(answer, maxAmount[i]);
+        }
+        return answer;
     }
 }
