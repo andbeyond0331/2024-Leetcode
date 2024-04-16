ed95b85b66b6bff9c785cdd292a91429d95324f6
PARK MI JUNG <88870452+andbeyond0331@users.noreply.github.com>
2024-04-16 21:28:27 +0900

    Time: 0 ms, Memory: 40.8 MB - LeetHub

--- house-robber/house-robber.java
+++ house-robber/house-robber.java
@@ -1,7 +1,7 @@
 class Solution {
     public int rob(int[] nums) {
-        // 3차 풀이 2024.04.12
-        int len = nums.length;
+        // 4차 풀이 2024.04.16
+       int len = nums.length;
         if(len == 1) {
             return nums[0];
         }
@@ -12,12 +12,12 @@ class Solution {
         }
         maxAmount[1] = nums[1];
         if(len == 3) {
-            return Math.max(nums[1], nums[0]+nums[2]);
+            return Math.max(nums[0] + nums[2], nums[1]);
         }
-        maxAmount[2] = Math.max(nums[1], nums[0] + nums[2]);
+        maxAmount[2] = Math.max(nums[0] + nums[2], nums[1]);
         int answer = -1;
         for(int i = 3; i < len; i++) {
-            maxAmount[i] = Math.max(maxAmount[i-1], Math.max(maxAmount[i-2], maxAmount[i-3]) + nums[i]);
+            maxAmount[i] = Math.max(maxAmount[i-1], Math.max(maxAmount[i-3], maxAmount[i-2]) + nums[i]);
             answer = Math.max(answer, maxAmount[i]);
         }
         return answer;
